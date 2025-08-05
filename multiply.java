3e9a4c50637cf005adefc25eed0bfa377de2fee5
Ashutosh Patel <ashutoshp141@gmail.com>
2025-08-05 12:48:29 +0530

    adj

---
+++ multiply.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class multiply {
+
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the number you want");
+            int num1 = sc.nextInt();
+            System.out.println("enter the number you want");
+            int num2 = sc.nextInt();
+            int multiply = num1 * num2;
+            System.out.println(multiply);
+        
+
+    }
+}
+ 
